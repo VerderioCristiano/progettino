244db4da33479a45f91fdb6327fdb8e1e95df950
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2024-12-05 08:45:14 +0000

    finito progetttino 1

--- progettino2/app.py
+++ progettino2/app.py
@@ -9,7 +9,7 @@ lista_spesa = []
 #rotta principale
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', lista = lista_spesa)
 
 
 @app.route('/aggiungi', methods=['POST'])
@@ -18,6 +18,13 @@ def aggiungi():
     if elemento:
         lista_spesa.append(elemento)
     return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
+
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
