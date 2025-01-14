1cb46c81f44f6028c430b637e9d00f5bb963da71
Cristiano Verderio <verderio.cristiano@iisgalvanimi.edu.it>
2025-01-14 08:52:37 +0000

    finito

---
+++ NASA/app.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template, redirect, url_for
+import requests
+app = Flask(__name__)
+NASA_API_KEY = 'VxYup1QihuUTeig45fsgYdznsIrVcade44hhe2Yq' #api kei della nasa
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #ourte /nasa per immagine NASA
+@app.route('/nasa')
+def nasa_home():
+    response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}')
+#richiedo i dati
+    data = response.json() #converte da json a dict python
+    return render_template('nasa.html', title=data.get("title"), description=data.get("explanation"), image_url=data.get("url"))
+
+#route /cat per fatto sui gatti
+@app.route('/cat')
+def cat_home():
+    response = requests.get('https://catfact.ninja/fact') #richiedo i dati
+    data = response.json() #converte da json a dict python
+    return render_template('cat_fact.html', fact=data.get("fact"))
